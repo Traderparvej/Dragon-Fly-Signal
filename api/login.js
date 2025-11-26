e03415ae3efe7d19ca258bf51e9de37c78767d61
Traderparvej <trederparvej@gmail.com>
2025-11-26 20:31:59 +0600

    Update login.js

--- api/login.js
+++ api/login.js
@@ -1,17 +1,14 @@
 export default function handler(req, res) {
   try {
-
     if (req.method !== "POST") {
       return res.status(405).json({ success: false, message: "Method not allowed" });
     }
 
-    // Ensure body exists
     if (!req.body) {
       return res.status(400).json({ success: false, message: "Invalid body" });
     }
 
     const { username, password } = req.body;
-
     const CORRECT_USER = "admin";
     const CORRECT_PASS = "11115";
 
@@ -20,7 +17,6 @@ export default function handler(req, res) {
     } else {
       return res.status(200).json({ success: false, message: "Wrong credentials" });
     }
-
   } catch (err) {
     return res.status(500).json({ success: false, message: "Server error" });
   }
